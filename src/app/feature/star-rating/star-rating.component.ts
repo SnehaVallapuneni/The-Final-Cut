adf372ae6d1f80f9609ed8026f8266ac27cdb94f
SnehaVallapuneni <vallapunenisneha@gmail.com>
2025-04-24 20:49:32 +0530

    Updated star-rating component

--- src/app/feature/star-rating/star-rating.component.ts
+++ src/app/feature/star-rating/star-rating.component.ts
@@ -1,20 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, Input,  OnInit } from '@angular/core';
 @Component({
   selector: 'app-star-rating',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './star-rating.component.html',
   styleUrl: './star-rating.component.css'
 })
 export class StarRatingComponent implements OnInit {
 
-  @Input() rating: any;
-  @Input() isReadOnly: boolean = false;
+  @Input() rating = 0;
+  @Input() max = 5;
+  @Input() isReadOnly = false;
+
+  stars: number[] = [];
+
+  rate(value: number) {
+    if (!this.isReadOnly) {
+      this.rating = value;
+    }
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+    this.stars = Array(this.max).fill(0);
   }
 
 }
