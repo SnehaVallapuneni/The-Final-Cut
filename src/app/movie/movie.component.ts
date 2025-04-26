cb4f458a5348ef6abaff3115bc074a98786e1011
SnehaVallapuneni <vallapunenisneha@gmail.com>
2025-04-26 20:58:34 +0530

    Added features

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { StarRatingComponent } from '../feature/star-rating/star-rating.component';
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-movie',
   standalone: true,
-  imports: [],
+  imports: [StarRatingComponent, HeaderComponent],
   templateUrl: './movie.component.html',
   styleUrl: './movie.component.css'
 })
 export class MovieComponent {
 
+  type = '';
+  id = '';
+  url = '';
+  movies: any;
+  movie: any;
 }
